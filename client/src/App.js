b622636ba88d19ce24180a9722e2339de4300bdf
MrTj458 <mrtj458@gmail.com>
2018-09-17 16:26:25 -0600

    Added Menu

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom'
 import Menus from './components/Menus'
 import NoMatch from './components/NoMatch'
 import NavBar from './components/NavBar'
+import Menu from './components/Menu'
 
 const App = () => (
   <Fragment>
     <NavBar />
     <Switch>
       <Route exact path='/' component={Menus} />
+      <Route exact path='/menus/:id' component={Menu} />
       <Route component={NoMatch} />
     </Switch>
   </Fragment>
--- client/src/components/Menus.js
+++ client/src/components/Menus.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Redirect, Link } from 'react-router-dom'
+import axios from 'axios'
 
-const Menus = () => (
-  <h1>Menus</h1>
-)
+class Menus extends React.Component {
+  state = { menus: [] }
+
+  componentDidMount() {
+    axios.get('/api/menus')
+      .then(res => this.setState({ menus: res.data }))
+  }
+
+  render() {
+    const { menus } = this.state
+    return (
+      <ul>
+      { menus.map( m =>
+          <li key={m.id}>
+            <Link to={`/menus/${m.id}`}>{m.name}</Link>
+          </li>
+        )
+      }
+      </ul>
+    )
+  }
+}
 
 export default Menus
