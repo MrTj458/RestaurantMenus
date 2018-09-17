b9fc19bb6ed00e8dcd2c852abc106c9797823b78
MrTj458 <mrtj458@gmail.com>
2018-09-17 16:44:54 -0600

    Added Form.js

---
+++ client/src/components/Form.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class Form extends React.Component {
+  defaultValues = { name: '' }
+  state = {...this.defaultValues}
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const menu = { ...this.state }
+    this.props.submit(menu)
+    this.setState({ ...this.defaultValues })
+  }
+
+  handleChange = (e) => {
+    const { target: { name, value }} = e
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    const { name } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={this.handleChange}
+          required
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default Form
