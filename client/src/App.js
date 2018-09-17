30dd37fcb4721292cadef5088b4fd50a5539f60f
MrTj458 <mrtj458@gmail.com>
2018-09-17 16:06:41 -0600

    Added axios

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Menus from './components/Menus'
 import NoMatch from './components/NoMatch'
+import NavBar from './components/NavBar'
 
 const App = () => (
   <Fragment>
+    <NavBar />
     <Switch>
       <Route exact path='/' component={Menus} />
       <Route component={NoMatch} />
