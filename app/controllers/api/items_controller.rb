7cdeb6c6c212ab1ed9eb0efdec81f7e4244898e5
MrTj458 <mrtj458@gmail.com>
2018-09-17 15:33:22 -0600

    added controllers

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,16 +1,48 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_menu
+  before_action :set_item, only: []
+
   def index
+    render json: @menu.items.order(created_at: :desc)
   end
 
   def show
+    render json: @item
   end
 
   def create
+    item = @menu.items.new(item_params)
+
+    if item.save
+      render json: item
+    else
+      render json: { errors: item.errors }, status: 422
+    end
   end
 
   def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: { errors: @item.errors }, status: 422
+    end
   end
 
   def destroy
+    @item.destroy
+  end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:menu_id])
+  end
+
+  def set_item
+    @item = @menu.items.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
